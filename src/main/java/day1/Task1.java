47e8cfd252baf3eabbbba7d39b49e8fda832ec02
DmitryF17 <farodmin@yandex.ru>
2021-10-24 17:52:57 +0300

    проверка

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -3,7 +3,7 @@ package day1;
 public class Task1 {
     public static void main(String[] args) {
 int word = 0;
-while (word <10) {
+while (word < 10) {
     System.out.print("JAVA" + " ");
     word += 1;
 }
