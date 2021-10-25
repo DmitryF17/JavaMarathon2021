6544b5938c1a8462271d6122c5f938f8b32bf58d
DmitryF17 <farodmin@yandex.ru>
2021-10-25 23:08:29 +0300

    the task the third day

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,17 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner console = new Scanner(System.in);
+        while (true) {
+            double divisible = console.nextDouble();
+            double divider = console.nextDouble();
+            if (divider == 0) {
+                break;
+            }
+            System.out.println(divisible / divider);
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,18 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner console = new Scanner(System.in);
+        for (int time = 0; time != 5; time++) {
+            double divisible = console.nextDouble();
+            double divider = console.nextDouble();
+            if (divider == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(divisible / divider);
+        }
     }
 }
