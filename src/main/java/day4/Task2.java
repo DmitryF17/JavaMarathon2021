f379ad13a9df8af8a48a7e5af8a538145bfcfa47
DmitryF17 <farodmin@yandex.ru>
2021-10-27 18:22:16 +0300

    the task the forth day

--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -1,7 +1,43 @@
 package day4;
 
+import java.util.Random;
+
+// Создать новый массив размера 100
+// и заполнить его случайными числами из диапазона от 0 до 10000.
+// Затем, используя цикл for each вывести в консоль:
+// наибольший элемент массива
+// наименьший элемент массива
+// количество элементов массива, оканчивающихся на 0
+// сумму элементов массива, оканчивающихся на 0
+//
+//Использовать сортировку запрещено.
+
 public class Task2 {
     public static void main(String[] args) {
-
+        int max = 0;
+        int endzero = 0;
+        int sumendzero = 0;
+        Random random = new Random();
+        int[] numbers = new int[100];
+        for (int num = 0; num != 100; num++) {
+            numbers[num] = random.nextInt(10000);
+        }
+        int min = numbers[0];
+        for (int num : numbers) {
+            if (num <= min) {
+                min = num;
+            }
+            if (num >= max) {
+                max = num;
+            }
+            if (num % 10 == 0) {
+                sumendzero += num;
+                endzero++;
+            }
+        }
+        System.out.println("Наибольший эемент массива : " + max);
+        System.out.println("Наименьший элемент массива : " + min);
+        System.out.println("Количество элементов оканчивающихся на ноль : " + endzero);
+        System.out.println("Сумма элементов массива , оканчивающихся на ноль : " + sumendzero);
     }
 }
--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -1,7 +1,35 @@
 package day4;
 
+import java.util.Random;
+
+//Заполнить двумерный массив (матрицу) случайными числами от 0 до 50.
+// Размер матрицы задать m=12, n=8 (m - размерность по строкам,
+// n - размерность по колонкам).
+// В консоль вывести индекс строки, сумма чисел в которой максимальна.
+// Если таких строк несколько, вывести индекс последней из них.
 public class Task3 {
     public static void main(String[] args) {
-
+        int maxline = 0;
+        int indexline = 0;
+        int index = 0;
+        int sumcolumn = 0;
+        Random random = new Random();
+        int[][] matrix = new int[12][8];
+        for (int line = 0; line != 12; line++) {
+            for (int column = 0; column != 8; column++) {
+                matrix[line][column] = random.nextInt(50);
+            }
+        }
+        for (int[] matrixcolumn : matrix) {
+            for (int num : matrixcolumn) {
+                sumcolumn += num;
+            }
+            if (sumcolumn >= maxline) {
+                maxline = sumcolumn;
+                indexline = index;
+            }
+            index++;
+        }
+        System.out.println("Индекс строки , сумма чисел в которой максимальна : " + indexline);
     }
 }
