0e3653b0d784c6020a97fcfb195cc65ea8b8a745
DmitryF17 <farodmin@yandex.ru>
2021-11-01 22:00:28 +0300

    the task the nineth day

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -1,7 +1,13 @@
 package day9.Task1;
 
+//Создайте в методе main() класса Task1 объект класса Студент и объект класса Преподаватель.
+// Выведите в консоль название учебной группы у объекта-студента и название предмета у объекта-преподавателя.
+// Затем, вызовите printInfo() на объектах и посмотрите на результат.
 public class Task1 {
     public static void main(String[] args) {
-
+        Student student = new Student("Иван", "Химик");
+        Teacher teacher = new Teacher("Геннадий", "Химия");
+        student.printInfo();
+        teacher.printInfo();
     }
 }
