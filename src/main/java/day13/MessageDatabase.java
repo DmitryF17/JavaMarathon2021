1939c8d0d5a580ed94fd70afa38d35fc5168451f
DmitryF17 <farodmin@yandex.ru>
2021-11-07 11:19:21 +0300

    social network

---
+++ src/main/java/day13/MessageDatabase.java
@@ -0,0 +1,51 @@
+package day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Класс MessageDatabase должен иметь следующую структуру:
+//
+//Поля:
+//Статическое поле “сообщения” (англ. messages),
+// которое будет хранить список из сообщений (объектов класса Message).
+// Это поле должно инициализироваться пустым списком.
+// Этот список и есть наша условная “база данных”, которая хранит все сообщения в соц. сети.
+//
+//Конструктор:
+//Нет конструктора. Объекты класса MessageDatabase создаваться не будут (все методы и поля статические).
+//
+//Методы:
+//public static void addNewMessage(User u1, User u2, String text) -
+// этот метод “отправляет” новое сообщение от пользователя u1 пользователю u2 с текстом text.
+// Отправка в нашем контексте означает добавление сообщения в нашу “базу данных”.
+//public static List<Message> getMessages() - возвращает список всех сообщений в “базе данных”.
+//public static void showDialog(User u1, User u2) -
+// этот метод должен вывести цепочку сообщений (диалог) пользователей u1 и u2. Формат вывода должен быть таким:
+//
+//user1: Привет!
+//user2: Привет, user1!
+//user1: Как у тебя дела?
+//user2: Все ок, спасибо :)
+//
+//* У вас могут быть любые другие сообщения. Где user1 и user2 - имена пользователей u1 и u2.
+public class MessageDatabase {
+    static List<Message> messages = new ArrayList<>();
+
+    public static void addNewMessage(User u1, User u2, String text) {
+        messages.add(new Message(u1, u2, text));
+    }
+
+    public static List<Message> getMessages() {
+        return messages;
+    }
+
+    public static void showDialog(User u1, User u2) {
+        for (Message message : messages) {
+            if (message.getSender().equals(u1) & message.getReceiver().equals(u2) |
+                    message.getSender().equals(u2) & message.getReceiver().equals(u1)) {
+                System.out.println(message.getSender().getUsername() + ": " + message.getText());
+            }
+        }
+
+    }
+}
--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -1,7 +1,26 @@
 package day13;
-
+//В методе main() этого класса необходимо создать трех пользователей.
+//Затем необходимо:
+//Отправить 2 сообщения от пользователя 1 пользователю 2
+//Отправить 3 сообщения от пользователя 2 пользователю 1
+//Отправить 3 сообщения от пользователя 3 пользователю 1
+//Отправить 3 сообщения от пользователя 1 пользователю 3
+//Отправить 1 сообщение от пользователя 3 пользователю 1
+//
+//Сообщения могут быть любыми.
+//После этого, необходимо вывести диалог пользователя 1 и пользователя 3 с помощью метода showDialog().
 public class Task1 {
     public static void main(String[] args) {
+        User user1 = new User("user1");
+        User user2 = new User("user2");
+        User user3 = new User("user3");
+        user1.sendMessage(user2, "Good mornind");
+        user1.sendMessage(user2, "How are you");
+        user2.sendMessage(user1, "Hello");
+        user2.sendMessage(user1, "It's OK");
 
+        MessageDatabase.showDialog(user1, user2);
+
+        MessageDatabase.showDialog(user2, user1);
     }
 }
