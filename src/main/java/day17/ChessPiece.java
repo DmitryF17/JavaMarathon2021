7e6ae0615c30a9c1060e7859c8a0a18f1a933664
DmitryF17 <farodmin@yandex.ru>
2021-11-15 22:46:29 +0300

    the task of the seventeenth days

---
+++ src/main/java/day17/ChessPiece.java
@@ -0,0 +1,22 @@
+package day17;
+
+public enum ChessPiece {
+    KING_WHITE(100, "♔"), KING_BLACK(100, "♚"),
+    QEEN_WHITE(9, "♕"), QEEN_BLACK(9, "♛"),
+    ROOK_WHITE(5, "♖"), ROOK_BLACK(5, "♜"),
+    BISHOP_WHITE(3.5, "♗"), BISHOP_BLACK(3.5, "♝"),
+    KNIGHT_WHITE(3, "♘"), KNIGHT_BLACK(3, "♞"),
+    PAWN_WHITE(1, "♙"), PAWN_BLACK(1, "♟️");
+
+private double valueChessPiece;
+private String chessPiece;
+
+    ChessPiece(double valueChessPiece, String chessPiece) {
+        this.valueChessPiece = valueChessPiece;
+        this.chessPiece = chessPiece;
+    }
+
+    public String getChessPiece() {
+        return chessPiece;
+    }
+}
