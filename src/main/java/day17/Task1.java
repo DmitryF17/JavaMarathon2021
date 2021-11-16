b5441b62e21ea564c86a5fdbd4f3cc240bdd40b6
DmitryF17 <farodmin@yandex.ru>
2021-11-16 22:45:24 +0300

    the task of the seventeenth days

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -2,6 +2,14 @@ package day17;
 
 public class Task1 {
     public static void main(String[] args) {
+        ChessPiece[] chess = {ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE,
+                ChessPiece.PAWN_WHITE, ChessPiece.PAWN_WHITE,
+                ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK,
+                ChessPiece.ROOK_BLACK, ChessPiece.ROOK_BLACK
+        };
 
+        for (ChessPiece piece : chess) {
+            System.out.print(piece.getChessPiece() + " ");
+        }
     }
 }
