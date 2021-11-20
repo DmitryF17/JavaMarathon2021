25ed2ede9f81dee2d7b7415fb1a919113d773d8f
DmitryF17 <farodmin@yandex.ru>
2021-11-20 13:37:12 +0300

    the task of the eighteenth days

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -1,7 +1,18 @@
 package day18;
 
 public class Task1 {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int[] numbers = {1, 2};
+
+        System.out.println(recursionSum(numbers, 0));
+    }
+
+    public static int recursionSum(int[] numbers, int index) {
+
+        if (index == numbers.length) {
+            return 0;
+        }
+        return numbers[index] + recursionSum(numbers, index + 1);
     }
 }
