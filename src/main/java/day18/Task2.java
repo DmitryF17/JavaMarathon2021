ce86997184322d7eaff4c0123d752dd2c85a8c9c
DmitryF17 <farodmin@yandex.ru>
2021-11-20 17:21:41 +0300

    the task of the eighteenth days

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -2,6 +2,17 @@ package day18;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        int number = 878878;
+        System.out.println(count7(number));
     }
+    public static int count7(int number) {
+        if (number == 0) {
+            return 0;
+        }
+        if (number % 10 == 7) {
+            return 1 + count7(number / 10);
+        } else {
+            return count7(number / 10);
+        }
+   }
 }
